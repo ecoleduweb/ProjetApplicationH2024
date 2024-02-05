5718bf223c3e9691b46488168ec61d6e51c54934
FelixBeauX54 <beaulieu.felixcr76@gmail.com>
2024-02-05 17:30:43 -0500

    Update du login

--- front-end/projet_application/src/routes/login/+page.svelte
+++ front-end/projet_application/src/routes/login/+page.svelte
@@ -1,52 +1,65 @@
 <script>
-    let error = "";
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const formData = new FormData(form);
-        const username = formData.get('username');
-        const password = formData.get('password');
+  import Link from "../../Components/Inputs/Link.svelte";
+  let error = "";
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const username = formData.get("username");
+    const password = formData.get("password");
 
-        console.log(username, password);
+    console.log(username, password);
 
-        const response = await fetch('http://localhost:5000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        });
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
 
-        if (response.ok) {
-            const data = await response.json();
-            localStorage.setItem('token', data.token);
-            location.href = '/dashboard';
-        } else {
-            error = await response.json();
-            console.log(error);
-        }
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      location.href = "/dashboard";
+    } else {
+      error = await response.json();
+      console.log(error);
     }
+  };
 </script>
 
 <svelte:head>
-	<title>Login</title>
-    <meta name="description" content="Login page">
-    <link rel="stylesheet" href="src/styles/login.css">
+  <title>Login</title>
+  <meta name="description" content="Login page" />
+  <link rel="stylesheet" href="src/styles/login.css" />
 </svelte:head>
 
 <section>
-	<div class="login">
-		<h1>Login</h1>
-		<form on:submit={handleSubmit} class="login-form">
-			<label for="username">Username</label>
-			<input type="text" id="username" name="username" required>
-			<label for="password">Password</label>
-			<input type="password" id="password" name="password" required>
-            {#if error}
-                <p>{error}</p>
-            {/if}
-			<button type="submit">Login</button>
-            <label for="register">Don't have an account? <a href="/register">Register</a></label>
-		</form>
-	</div>
+  <div class="login">
+    <h1>Login</h1>
+    <form on:submit={handleSubmit} class="login-form">
+      <label for="username">Username</label>
+      <input
+        type="text"
+        class="input-login"
+        id="username"
+        name="username"
+        required
+      />
+      <label for="password">Password</label>
+      <input
+        type="password"
+        class="input-login"
+        id="password"
+        name="password"
+        required
+      />
+      {#if error}
+        <p>{error}</p>
+      {/if}
+      <button type="submit" class="input-login">Se connecter </button>
+      <Link href="register" text="Register" />
+    </form>
+  </div>
 </section>
--- front-end/projet_application/src/routes/register/+page.svelte
+++ front-end/projet_application/src/routes/register/+page.svelte
@@ -10,6 +10,7 @@
   let email = "";
   let password = "";
   let validatePassword = "";
+  let nomEntreprise = "";
 
   import Button from "../../Components/Inputs/Button.svelte";
 
@@ -33,46 +34,64 @@
   <h1>Créer un compte</h1>
 
   <form on:submit|preventDefault={handleSubmit} class="form-register">
-    <div class="form-inputs">
-      <label for="firstName">Prénom</label>
-      <input type="text" id="firstName" bind:value={firstName} required />
+    <div class="info-block">
+      <h2>Information personnelle</h2>
+      <div class="form-inputs">
+        <label for="firstName">Prénom</label>
+        <input type="text" id="firstName" bind:value={firstName} required />
+      </div>
+      <div class="form-inputs">
+        <label for="lastName">Nom de famille</label>
+        <input type="text" id="lastName" bind:value={lastName} required />
+      </div>
     </div>
-    <div class="form-inputs">
-      <label for="lastName">Nom de famille</label>
-      <input type="text" id="lastName" bind:value={lastName} required />
+    <div class="info-block">
+      <h2>Information de l'entreprise</h2>
+      <div class="form-inputs">
+        <label for="entreprise">Nom Entreprise</label>
+        <input
+          type="text"
+          id="entreprise"
+          bind:value={nomEntreprise}
+          required
+        />
+      </div>
+      <div class="form-inputs">
+        <label for="address">Adresse</label>
+        <input type="text" id="address" bind:value={address} required />
+      </div>
+      <div class="form-inputs">
+        <label for="city">Ville</label>
+        <input type="text" id="city" bind:value={city} required />
+      </div>
+      <div class="form-inputs">
+        <label for="zipCode">Code Postal</label>
+        <input type="text" id="zipCode" bind:value={zipCode} required />
+      </div>
+      <div class="form-inputs">
+        <label for="province">Province</label>
+        <input type="text" id="province" bind:value={province} required />
+      </div>
     </div>
-    <div class="form-inputs">
-      <label for="address">Adresse</label>
-      <input type="text" id="address" bind:value={address} required />
-    </div>
-    <div class="form-inputs">
-      <label for="city">Ville</label>
-      <input type="text" id="city" bind:value={city} required />
-    </div>
-    <div class="form-inputs">
-      <label for="zipCode">Code Postal</label>
-      <input type="text" id="zipCode" bind:value={zipCode} required />
-    </div>
-    <div class="form-inputs">
-      <label for="province">Province</label>
-      <input type="text" id="province" bind:value={province} required />
-    </div>
-    <div class="form-inputs">
-      <label for="email">Courriel</label>
-      <input type="email" id="email" bind:value={email} required />
-    </div>
-    <div class="form-inputs">
-      <label for="password">Mot de passe</label>
-      <input type="password" id="password" bind:value={password} required />
-    </div>
-    <div class="form-inputs">
-      <label for="password">Valider Mot de passe</label>
-      <input
-        type="password"
-        id="confirm_password"
-        bind:value={validatePassword}
-        required
-      />
+    <div class="info-block">
+      <h2>Information de connexion</h2>
+      <div class="form-inputs">
+        <label for="email">Courriel</label>
+        <input type="email" id="email" bind:value={email} required />
+      </div>
+      <div class="form-inputs">
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" bind:value={password} required />
+      </div>
+      <div class="form-inputs">
+        <label for="password">Valider Mot de passe</label>
+        <input
+          type="password"
+          id="confirm_password"
+          bind:value={validatePassword}
+          required
+        />
+      </div>
     </div>
     <div class="form-inputs form-buttons">
       <Button on:click text="Créer" />
