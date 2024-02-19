b1aba7c57f7bd9f0d719cff3b56667519a30b511
Rino Gamache <2035718@cegeprdl.ca>
2024-02-19 17:00:01 -0500

    Modification du formulaires

--- front-end/projet_application/src/Models/Offre.ts
+++ front-end/projet_application/src/Models/Offre.ts
@@ -3,22 +3,16 @@
 export interface Offre { 
     titre: string; // titre de l'offre
     lieu: string; // lieu de travail
-    active: boolean; // si l'offre est active ou non
     dateEntreeFontion: Date // date d'entrée en fonction
     dateLimitePostuler: Date // date limite pour postuler
-
-    heure: string; // heure par semaine
-    salaire: string; // salaire de lheure 
-    stage: boolean; // si stage vrai sinon faux
-    duree: string; // temps partiel, temps plein
+    salaire: number; // salaire de lheure 
+    typeEmplois: string; // si stage vrai sinon faux
+    heures: number; // nombre d'heures par semaine  
     programme: string; // le programme visée par l'offre
     conciliation: boolean; // employeur conciliant ou non
     urgente: boolean; // si l'offre est urgente
     lien: string; // lien vers l'offre ou site web de l'employeur
     description: string; // description de l'offre
-    dateAffichageDebut: string; // date de début de l'affichage
-    dateAffichageFin: string; // date de fin de l'affichage
-    personneContact: string; // nom de la personne à contacter
     courrielContact: string; // courriel de la personne à contacter
-    telephoneContact: string; // téléphone de la personne à contacter
-} 
+    active: boolean; // si l'offre est active ou non
+}
--- front-end/projet_application/src/routes/offre/+page.svelte
+++ front-end/projet_application/src/routes/offre/+page.svelte
@@ -1,32 +1,61 @@
 <script lang="ts">
     import "../../styles/global.css";
     import "../../styles/offre.css";
-  
     import Button from "../../Components/Inputs/Button.svelte";
-    // import { MultiSelect } from "carbon-components-svelte"; -----------------
     import MultiSelect from 'svelte-multiselect';
     import type { Offre } from "../../Models/Offre";
+    import { POST } from "../../ts/server";
+    import * as yup from "yup";
+    import { extractErrors } from "../../ts/utils";
   
+    const schema = yup.object().shape({
+    titre: yup.string().required("Le titre du poste est requis"),
+    lieu: yup.string().required("Le lieu de travail est requis"),
+    description: yup.string().required("La description de l'offre est requise"),
+    dateEntreeFontion: yup.date().required("La date d'entrée en fonction est requise").max(new Date(), "La date d'entrée doit être valide"),
+    dateLimitePostuler: yup.date().required("La date limite pour postuler est requise")
+    .min(new Date(), "La date d'entrée doit être valide et dans le futur"),
+    salaire: yup.number().required("Le salaire est requis").min(0, "Le salaire doit être positif"),
+    typeEmplois: yup.array().of(yup.number()).required("Le type d'emplois est requis"),
+    courrielContact: yup.string().email("Le courriel de contact n'est pas valide").required("Le courriel de contact est requis"),
+    
+  });
+
     let offre: Offre = {
     titre: "",
     lieu: "",
     dateEntreeFontion: new Date(),
-    heure : "",
-    salaire: "",
-    stage: true,
-    duree: "",
+    dateLimitePostuler: new Date(),
+    salaire: 0,
+    typeEmplois: "",
+    heures: 0,
     programme: "",
     conciliation: false, 
     urgente: false,
     lien: "",
     description: "",
-    dateAffichageDebut: "", 
-    dateAffichageFin: "",
-    personneContact: "",
     courrielContact: "",
-    telephoneContact: "",
     active: true,
     };
+
+    let errors: Offre = {
+    titre: "",
+    lieu: "",
+    dateEntreeFontion: new Date(),
+    dateLimitePostuler: new Date(),
+    salaire: 0,
+    typeEmplois: "",
+    heures: 0,
+    programme: "",
+    conciliation: false, 
+    urgente: false,
+    lien: "",
+    description: "",
+    courrielContact: "",
+    active: true,
+    };
+
+
 
     let programmeSelected: { label: string; value: number }[] = [];
     let programmes = [
@@ -47,21 +76,46 @@
   ];
 
 
-    function handleSubmit() {
-      // Here you can handle form submission, for now, just logging the values
-      console.log(programmeSelected);
+
+  const handleSubmit = async () => {
+    try {
+      // `abortEarly: false` to get all the errors
+      await schema.validate(offre, { abortEarly: false });
+      errors = {
+        titre: "",
+        lieu: "",
+        dateEntreeFontion: new Date(),
+        dateLimitePostuler: new Date(),
+        salaire: 0,
+        typeEmplois: "",
+        heures: 0,
+        programme: "",
+        conciliation: false, 
+        urgente: false,
+        lien: "",
+        description: "",
+        courrielContact: "",
+        active: true,
+      };
+
       console.log(offre);
+      const response = POST("/auth/login", offre); // PATH DE L'AJOUT DE L'OFFRE A MODIFIER
+      console.log(response);
+    } catch (err) {
+      errors = extractErrors(err);
     }
+  };
+
   </script>
   
   <div class="container">
     <form on:submit|preventDefault={handleSubmit} class="form-offre">
       <h1>Créer une nouvelle offre d'emploi</h1>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="titre">Titre du poste*</label>
         <input type="text" bind:value={offre.titre} class="form-control" id="titre" />
       </div>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="typeEmplois">Type d'emplois*</label>
         <MultiSelect
           id="periode"
@@ -70,27 +124,19 @@
           bind:value={typeEmploisSelected}
         />
       </div>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="lieu">Lieu de travail*</label>
         <input type="text" bind:value={offre.lieu} class="form-control" id="salaire" />
       </div>
-      <div class="form-group-horizontal">
-        <label for="description">Description du poste*</label>
-        <input type="text" bind:value={offre.description} class="form-control" id="salaire" />
-      </div>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="dateEntree">Date d'entrée en fonction</label>
         <input type="date" bind:value={offre.dateEntreeFontion} class="form-control" id="dateEntree" />
       </div>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="dateLimite">Date limite pour postuler</label>
         <input type="date" bind:value={offre.dateLimitePostuler} class="form-control" id="dateLimite" />
       </div>
-      <div class="form-group-horizontal">
-        <label for="duree">Durée</label>
-        <input type="text" bind:value={offre.duree} class="form-control" id="duree" />
-      </div>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="duree">Programme visée</label>
         <MultiSelect
           id="programme"
@@ -108,10 +154,18 @@
         <label for="urgente">Urgente</label>
         <input type="checkbox" bind:checked={offre.urgente} class="form-control" id="urgente" />
       </div>
-      <div class="form-group-horizontal">
+      <div class="form-group-vertical">
         <label for="lien">Lien</label>
+        <input type="text" bind:value={offre.lien} class="form-control" id="lien" />
+      </div>
+      <div class="form-group-vertical">
+        <label for="courriel-contact">Courriel contact</label>
+        <input type="text" bind:value={offre.courrielContact} class="form-control" id="courriel-contact" />
+      </div>
+      <div class="form-group-vertical">
+        <label for="description">Description du poste*</label>
+        <textarea rows="15" cols="50" bind:value={offre.description} class="form-control" id="description" />
+      </div>
       <Button text="Enregistrer" on:click={() => handleSubmit()} />
     </form>
-    
   </div>
-  
