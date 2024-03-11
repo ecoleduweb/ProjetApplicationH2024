61d0dd08ef885beb931facb2fd877e2e440a01c3
FelixBeauX54 <beaulieu.felixcr76@gmail.com>
2024-03-11 09:01:15 -0400

    Ajout du token

--- front-end/projet_application/src/routes/+page.svelte
+++ front-end/projet_application/src/routes/+page.svelte
@@ -2,20 +2,57 @@
   import "../styles/global.css";
   import Header from "../Components/Common/Header.svelte";
   import Footer from "../Components/Common/Footer.svelte";
+  import Button from "../Components/Inputs/Button.svelte";
 </script>
 
 <Header />
-<br />
-<br />
-<br />
-<br />
-<h1>PageH1</h1>
-<h2>PageH2</h2>
+<main>
+  <section class="haut">
+    <div class="haut-gauche">
+      <h1 class="title">
+        <span>PORTAIL D'</span><span>OFFRES D'EMPLOI</span>
+      </h1>
+      <h2>DU CÉGEP DE RIVIÈRE-DU-LOUP</h2>
+    </div>
+    <div class="haut-droite">
+      <Button text="Consulter toutes les offres" />
+    </div>
+  </section>
+</main>
 <Footer />
 
 <style scoped>
-  h1,
   h2 {
     color: white;
+  }
+  .title {
+    left: 7.2%;
+  }
+  .title span:first-child {
+    color: white;
+    margin: 0;
+  }
+  .title span:last-child {
+    color: #00ad9a;
+    margin: 0;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  .haut {
+    display: flex;
+    widows: 85%;
+  }
+  .haut-gauche {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    left: 7.2%;
+  }
+  .haut-droite {
+    width: 50%;
   }
 </style>
