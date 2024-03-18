5112da439fbd6d60dbab7d602108a30f6a8a4817
FelixBeauX54 <beaulieu.felixcr76@gmail.com>
2024-03-18 14:49:27 -0400

    Finalisation du modal pour afficher une offre d'emploi

--- front-end/projet_application/src/Components/Common/Modal.svelte
+++ front-end/projet_application/src/Components/Common/Modal.svelte
@@ -10,21 +10,27 @@
 <button class="slot" on:click={handleButtonClick}>
     <button class="modal" on:click|stopPropagation>
         <slot />
+        <button class="close" on:click={handleButtonClick}>
+            <img src="cancel.svg" class="image" alt="close" />
+        </button>
     </button>
 </button>
 
-<style scoped>
+<style scoped lang="scss">
     .slot {
         width: 100%;
         height: 100%;
         position: fixed;
         top: 0;
         left: 0;
-        background-color: rgba(0, 0, 0, 0.5); /* semi-transparent overlay */
+        background-color: rgba(0, 0, 0, 0.5);
         display: flex;
         justify-content: center;
         align-items: center;
         border: none;
+    }
+    .slot:hover {
+        cursor: default;
     }
     .modal {
         background-color: #e5e5e5;
@@ -36,5 +42,26 @@
         padding: 20px;
         box-sizing: border-box;
         z-index: 100;
+        display: flex;
+        justify-content: center;
+    }
+    button {
+        border: none;
+        background-color: transparent;
+        :hover {
+            cursor: default;
+        }
+    }
+    .close {
+        width: 8%;
+        border: none;
+        background-color: transparent;
+        :hover {
+            cursor: pointer;
+        }
+    }
+
+    .image {
+        width: 100%;
     }
 </style>
--- front-end/projet_application/src/Components/OffreEmplois/OffreEmploi.svelte
+++ front-end/projet_application/src/Components/OffreEmplois/OffreEmploi.svelte
@@ -7,31 +7,68 @@
 
 <Modal handleModalClick={handleEmploiClick}>
     <div class="container">
+        <div class="titleContainer">
+            <h3 class="title">{emploi.titre}</h3>
+            <h4 class="subtitle">{emploi.entreprise}</h4>
+        </div>
         <div class="info">
+            <h5 class="infoTitle">Type de poste</h5>
             <p class="text">{emploi.titre}</p>
-            <p class="text">{emploi.entreprise}</p>
-            <p class="text">{emploi.dateFin}</p>
+            <h5 class="infoTitle">Adresse du lieu de travaiL</h5>
+            <p class="text">{emploi.ville}</p>
+            <h5 class="infoTitle">Description du poste</h5>
             <p class="text">{emploi.description}</p>
+            <h5 class="infoTitle">Date de début</h5>
+            <p class="text">{emploi.dateDebut}</p>
+            <h5 class="infoTitle">Date limite pour postuler</h5>
+            <p class="text">{emploi.dateFin}</p>
+            <h5 class="infoTitle">Nom de la personne responsable</h5>
+            <p class="text">nom de la personne à ajouter</p>
+            <h5 class="infoTitle">Où envoyer votre candidature</h5>
+            <p class="text">Courriel de la personne à ajouter</p>
         </div>
     </div>
 </Modal>
 
-<style>
-    .info {
+<style scoped>
+    .titleContainer {
+        display: flex;
+        flex-direction: column;
+    }
+    .title {
+        font-size: 2.5rem;
+        color: #00ad9a;
+        margin: 0px;
+        margin-bottom: 1.5vw;
+    }
+    .subtitle {
+        font-size: 1.5rem;
+        margin: 0px;
+        margin-bottom: 2.25vw;
+        color: black;
+    }
+    .infoTitle {
         font-size: 1.3rem;
-        margin-bottom: 20px;
+        margin: 0px;
+        margin-bottom: 0.5vw;
+    }
+    .info {
         color: black;
     }
     .text {
-        margin-right: 10px;
+        font-size: 1.1rem;
+        margin: 0px;
+        margin-bottom: 1.75vw;
+        color: black;
     }
     .container {
+        width: 95%;
         display: flex;
+        flex-direction: column;
+        text-align: left;
         justify-content: space-between;
-        align-items: center;
         color: white;
         border-radius: 4px;
-        cursor: pointer;
         transition: background-color 0.3s ease;
     }
 </style>
